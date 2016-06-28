352c1633ef56924bb8ed2220c523f6ef1a1420d4
rajthegreat80 <rajthegreat80@users.noreply.github.com>
2016-06-28 12:11:28 +0530

    Update wfh.py

--- wfh.py
+++ wfh.py
@@ -26,7 +26,7 @@ import mimetypes
 
 SCOPES = 'https://www.googleapis.com/auth/gmail.send'
 CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'sfValidator'
+APPLICATION_NAME = '<Application Name>'
 
 def get_credentials():
     """Gets valid user credentials from storage.
